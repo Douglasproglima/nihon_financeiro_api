507d82e6af0067cc7338f0e17a949d62e5b28304
Douglas Lima <douglasproglima@gmail.com>
2018-06-03 20:55:19 +0000

    Alteração de aspas duplas para aspas simples

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -43,5 +43,5 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
   it { is_expected.to validate_confirmation_of(:password) }
-  it { is_expected.to allow_value("admin@admin.com").for(:email) }
+  it { is_expected.to allow_value('admin@admin.com').for(:email) }
 end
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -6,30 +6,30 @@ RSpec.describe 'Usuários da API', type: :request do
   let!(:user){ create(:user) }
   let(:user_id){ user.id }
 
-  before { host! "api.localhost.test" }
+  before { host! 'api.localhost.test' }
   # before { host! "api.nihon_financeiro.test" }
 
-  describe "GET /users/:id" do
+  describe 'GET /users/:id' do
     before do
-      headers = { "Accept" => "application/vnd.nihon_financeiro.v1" }
+      headers = { 'Accept' => 'application/vnd.nihon_financeiro.v1' }
       get "/users/#{user_id}", params: {}, headers: headers
     end
 
-    context "Quando o usuário não existir" do
-      it "Retorna o usuário" do
+    context 'Quando o usuário não existir' do
+      it 'Retorna o usuário' do
         user_response = JSON.parse(response.body)
         expect(user_response["id"]).to eq(user_id)
       end
 
-      it "Retorna o código status: 200 OK" do
+      it 'Retorna o código status: 200 OK' do
         expect(response).to have_http_status(200)
       end
 
-      it "Quando o usuário não existir" do
+      it 'Quando o usuário não existir' do
         let(:user_id) {1000}
       end
 
-      it "Retorna o código status: 404 ERROR" do
+      it 'Retorna o código status: 404 ERROR' do
         expect(response).to have_http_status(404)
       end
     end
