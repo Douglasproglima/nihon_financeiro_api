0e0f2d6c40306c08c390e9f4aa73095bc7c0e20d
Douglas Lima <douglasproglima@gmail.com>
2018-06-06 09:12:16 +0000

    Retorando a autorização e add o método user_signed_on()

--- spec/controllers/concerms/authenticable_spec.rb
+++ spec/controllers/concerms/authenticable_spec.rb
@@ -23,48 +23,47 @@ RSpec.describe Authenticable do
     end
   end
 
+  describe '#authenticate_with_token!' do
+    controller do
+      before_action :authenticate_with_token!
 
-  # describe '#authenticate_with_token!' do
-  #   controller do
-  #     before_action :authenticate_with_token!
-  #
-  #     def restricted_action; end
-  #   end
-  #
-  #   context 'when there is no user logged in' do
-  #     before do
-  #       allow(app_controller).to receive(:current_user).and_return(nil)
-  #       routes.draw { get 'restricted_action' => 'anonymous#restricted_action' }
-  #       get :restricted_action
-  #     end
-  #
-  #     it 'Retorna o código status: 401' do
-  #       expect(response).to have_http_status(401)
-  #     end
-  #
-  #     it 'returns the json data for the errors' do
-  #       expect(json_body).to have_key(:errors)
-  #     end
-  #   end
-  # end
-  #
-  #
-  # describe '#user_logged_in?' do
-  #   context 'when there is a user logged in' do
-  #     before do
-  #       user = create(:user)
-  #       allow(app_controller).to receive(:current_user).and_return(user)
-  #     end
-  #
-  #     it { expect(app_controller.user_logged_in?).to be true }
-  #   end
-  #
-  #   context 'when there is no user logged in' do
-  #     before do
-  #       allow(app_controller).to receive(:current_user).and_return(nil)
-  #     end
-  #
-  #     it { expect(app_controller.user_logged_in?).to be false }
-  #   end
-  # end
+      def restricted_action; end
+    end
+
+    context 'Quando não tiver nenhum usuário logado no sistema' do
+      before do
+        allow(app_controller).to receive(:current_user).and_return(nil)
+        routes.draw { get 'restricted_action' => 'anonymous#restricted_action' }
+        get :restricted_action
+      end
+
+      it 'Retorna o código status: 401' do
+        expect(response).to have_http_status(401)
+      end
+
+      it 'Retorna um JSON os dados de erros.' do
+        expect(json_body).to have_key(:errors)
+      end
+    end
+  end
+
+
+  describe '#user_logged_in?' do
+    context 'Quando tiver um usuário logado no sistema.' do
+      before do
+        user = create(:user)
+        allow(app_controller).to receive(:current_user).and_return(user)
+      end
+
+      it { expect(app_controller.user_logged_in?).to be true }
+    end
+
+    context 'Quando não tiver nenhum usuário logado no sistema' do
+      before do
+        allow(app_controller).to receive(:current_user).and_return(nil)
+      end
+
+      it { expect(app_controller.user_logged_in?).to be false }
+    end
+  end
 end
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -8,7 +8,8 @@ RSpec.describe 'Usuários da API', type: :request do
   let(:headers) do
     {
         'Accept' => 'application/vnd.nihon_financeiro.v1',
-        'Content-Type' => Mime[:json].to_s
+        'Content-Type' => Mime[:json].to_s,
+        'Authorization' => user.auth_token
     }
   end
 
@@ -106,7 +107,7 @@ RSpec.describe 'Usuários da API', type: :request do
   #Verbo DELETE
   describe 'DELETE /users/:id' do
     before do
-      headers = { 'Accept' => 'application/vnd.nihon_financeiro.v1' }
+      # headers = { 'Accept' => 'application/vnd.nihon_financeiro.v1' }
       delete "/users/#{user_id}", params: {}, headers: headers
     end
 
